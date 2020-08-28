135cc8b3a0d0f41697a441cd6d51c738079d0254
shivamyth <docshm007@gmail.com>
2020-08-28 15:31:30 +0530

    try snapshot testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,11 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-storyshots",
+  ],
   webpackFinal: (config) => {
     return {
       ...config,
